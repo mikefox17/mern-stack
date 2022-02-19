e2c9e6bb72e15022e29cb947549c2cc99166fb34
mikefox17 <mikefox17@gmail.com>
2022-02-19 10:46:12 -0500

    register/login/logout working, fixed cors

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const { connectDB } = require('./config/db');
+const cors = require('cors');
 const port = process.env.PORT || 8000;
 
 connectDB();
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +16,17 @@ app.use(
         extended: false,
     })
 );
+//disable cors
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
